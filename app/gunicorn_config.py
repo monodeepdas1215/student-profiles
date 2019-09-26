1469fddb534e1544573b188c416254333162294a
Monodeep Das <monodeepdas1215@gmail.com>
2019-09-27 01:03:32 +0530

    gunicorn config updated

--- app/gunicorn_config.py
+++ app/gunicorn_config.py
@@ -2,8 +2,8 @@ import multiprocessing
 
 
 bind = "0.0.0.0:8081"
-# workers = multiprocessing.cpu_count() * 2 + 1
-workers = 1
+workers = multiprocessing.cpu_count() * 2 + 1
+# workers = 1
 
 reload = True
 
